08551a0a65e377441d39a1d088de963ff9c49e0c
Antonio Fiñana <antonio.finana@worldline.com>
2017-03-24 16:47:39 +0100

    update version

--- app/src/main/java/net/middleland/mybluecar/MainActivity.java
+++ app/src/main/java/net/middleland/mybluecar/MainActivity.java
@@ -1,20 +1,37 @@
+
 package net.middleland.mybluecar;
+
+
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import net.middleland.mybluecar.fragments.CameraFragment;
+import net.middleland.mybluecar.fragments.JoystickFragment;
+import net.middleland.mybluecar.fragments.SettingsFragment;
+import net.middleland.mybluecar.logger.Log;
+import net.middleland.mybluecar.logger.LogFragment;
+import net.middleland.mybluecar.logger.LogWrapper;
+import net.middleland.mybluecar.logger.MessageOnlyLogFilter;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +52,20 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.content_main, new JoystickFragment()).commit();
+        }
+
+
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -59,45 +84,128 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        switch (item.getItemId()){
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            handleCamera();
+            case R.id.nav_camera:{
+              // Handle the camera action
+              handleCamera();
+            }
 
-        } else if (id == R.id.nav_gallery) {
+            case R.id.nav_joystick: {
+                // Handle the joystick action
+                handleJoystick();
 
-        } else if (id == R.id.nav_slideshow) {
+           }
+           case R.id.nav_settings: {
+                // Handle the settings options
+                handleSettings();
+           }
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_logger:{
+                // Handler logger option
+                handleLog();
+            }
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        } 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
+
+/**
+ * Handle the Joystick action
+ */
+
+    private void handleJoystick() {
+        // Create new fragment and transaction
+        Fragment newFragment = new JoystickFragment();
+        replaceFragment( newFragment);
+    }
+
+
+/**
+ * Handle the camera action
+ */
+
+    private void handleCamera() {
+        // Create new fragment and transaction
+        Fragment newFragment = new CameraFragment();
+
+        replaceFragment(newFragment);
+    }
+
+
+/**
+ * Handle the Settings action
+ */
+   private void  handleSettings(){
+        // Create new fragment and transaction
+        Fragment newFragment = new SettingsFragment();
+
+        replaceFragment(newFragment);
+
+    }
+
+    /**
+     * Handle the Loggin action
+     */
+    private void  handleLog(){
+        // Create new fragment and transaction
+        Fragment newFragment = new LogFragment();
+
+        replaceFragment(newFragment);
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this
+        // fragment,
+        // and add the transaction to the back stack so the user can navigate
+        // back
+        transaction.replace(R.id.content_main, fragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+// Loggin stuff
+
+    /** Create a chain of targets that will receive log data */
+    @Override
+    public void initializeLogging() {
+        // Wraps Android's native log framework.
+        LogWrapper logWrapper = new LogWrapper();
+        // Using Log, front-end to the logging chain, emulates android.util.log method signatures.
+        Log.setLogNode(logWrapper);
+
+        // Filter strips out everything except the message text.
+        MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
+        logWrapper.setNext(msgFilter);
+
+        // On screen logging via a fragment with a TextView.
+        LogFragment logFragment = (LogFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.log_fragment);
+        msgFilter.setNext(logFragment.getLogView());
+
+        Log.i(TAG, "Ready");
+    }
+
+
 }
+
--- app/src/main/java/net/middleland/mybluecar/fragments/SettingsFragment.java
+++ app/src/main/java/net/middleland/mybluecar/fragments/SettingsFragment.java
@@ -1,27 +1,30 @@
 package net.middleland.mybluecar.fragments;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.middleand.mobile.rccar.R;
+import net.middleland.mybluecar.R;
 
 /**
- * A placeholder fragment containing Settings Application view.
+ * A simple {@link Fragment} subclass.
  */
-public class SettingFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    public SettingFragment() {
+
+    public SettingsFragment() {
+        // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_setting, container,
-                false);
-        return rootView;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
 }
